b26cd56e8b54a42457e10f6623528b9db42b1a04
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-01-15 22:24:47 +0200

    Get weather for all blocks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,8 +9,11 @@ $(function() {
         .then(updateLocalWeather);
 
     var cities = document.getElementsByClassName("another-city");
-
     Array.prototype.slice.call(cities).map(loadWeather);
+
+    $.when(getLocation())
+        .then(dailyForecast)
+        .then(renderForecast);
 });
 
 var yourCity = document.getElementsByTagName("h3")[0];
@@ -46,9 +49,37 @@ function loadWeather(cityElem) {
         .then(updateView);
 };
 
-function updateAnotherCityWeather(weather) {
+function dailyForecast(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var dailyApiUrl = forecastUrl(lon, lat);
 
-};
+    var deferred = $.Deferred();
+    $.getJSON(dailyApiUrl, deferred.resolve).fail(deferred.reject);
+    return deferred.promise();
+}
+
+function renderForecast(forecast) {
+    var daily = document.getElementsByClassName("daily-forecast");
+    daily = Array.prototype.slice.call(daily);
+    var forecastList = forecast.list.slice(1);
+    _.zip(daily, forecastList).map(function(pair) {
+        console.log(pair);
+        var el = pair[0];
+        var weather = pair[1];
+
+        var formattedDate = moment.unix(weather.dt).format("ll");
+
+        el.childNodes[1].innerHTML = formattedDate;
+        el.childNodes[3].innerHTML = parseInt(weather.speed) +
+            ' m/s';
+        el.childNodes[5].innerHTML = parseInt(weather.temp.day -
+                273.15) +
+            " C";
+        el.childNodes[7].innerHTML = weather.weather[0].main;
+
+    })
+}
 
 function getLocation() {
     var deferred = $.Deferred();
@@ -90,6 +121,16 @@ function makeApiUrl(lon, lat, appId) {
     return geocodingAPI;
 };
 
+function forecastUrl(lon, lat, days) {
+    days = days || 4
+    var geocodingDailyApi =
+        "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" +
+        lat +
+        "&lon=" + lon +
+        "&APPID=" + keyApi +
+        '&cnt=' + days;
+    return geocodingDailyApi;
+}
 
 function showError(error) {
     switch (error.code) {
